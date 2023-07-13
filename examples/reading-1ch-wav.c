a02d0d034ef205ac1675f25a0ad8196ca5b7d21c
Đorđe Jocić <office@djordjejocic.com>
2023-07-13 16:20:53 +0200

    Reading 1-CH Wav Example

---
+++ examples/reading-1ch-wav.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "c89-wav/format.h"
+
+void main() {
+    
+    WAV_FILE file = wav_open("/path/to/file.wav", WAV_RD);
+    
+    if (file.bin.open) {
+        
+        while (wav_has_next(&file)) { // Loop Through Samples
+            
+            int32_t sample = wav_next_sample(&file);
+            
+            // ...
+        }
+        
+        if (!wav_close(&file)) {
+            // Handle I/O Error
+        }
+    }
+}
